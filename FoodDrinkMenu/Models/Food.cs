045204e0a4172bfea33b4afb464d9af4e275a99a
Israel Calderon <icalderond@outlook.com>
2019-06-30 16:18:11 -0500

    Adding first part of burgers page

---
+++ FoodDrinkMenu/Models/Food.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodDrinkMenu.Models
+{
+    public class Food
+    {
+        public string Name { get; set; }
+        public string ImageSource { get; set; }
+        public string Price { get; set; }
+        public string ColorHex { get; set; }
+    }
+}
---
+++ FoodDrinkMenu/Views/Burgers.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FoodDrinkMenu.Views
+{
+    public partial class Burgers : ContentPage
+    {
+        public Burgers()
+        {
+            InitializeComponent();
+        }
+    }
+}
