b85d463d17d11a8bc1755f0006e5b13006b77041
Israel Calderon <icalderond@outlook.com>
2019-07-01 08:29:15 -0500

    Adding custom font

--- FoodDrinkMenu.iOS/Main.cs
+++ FoodDrinkMenu.iOS/Main.cs
@@ -12,16 +12,9 @@ namespace FoodDrinkMenu.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            try
-            {
-                // if you want to use a different Application Delegate class from "AppDelegate"
-                // you can specify it here.
-                UIApplication.Main(args, null, "AppDelegate");
-            }
-            catch (Exception ex)
-            {
-                var o = ex.Message;
-            }
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
         }
     }
 }
