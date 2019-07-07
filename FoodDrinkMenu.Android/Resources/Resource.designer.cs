019d19e584e7b812036982e6b131a6025423510d
Israel Calderon <icalderond@outlook.com>
2019-07-07 14:41:59 -0500

    Finished burgers page

--- FoodDrinkMenu.Android/Resources/Resource.designer.cs
+++ FoodDrinkMenu.Android/Resources/Resource.designer.cs
@@ -6444,13 +6444,16 @@ namespace FoodDrinkMenu.Droid
 			public const int notify_panel_notification_icon_bg = 2131165498;
 			
 			// aapt resource value: 0x7F07013B
-			public const int star = 2131165499;
+			public const int salad = 2131165499;
 			
 			// aapt resource value: 0x7F07013C
-			public const int tooltip_frame_dark = 2131165500;
+			public const int star = 2131165500;
 			
 			// aapt resource value: 0x7F07013D
-			public const int tooltip_frame_light = 2131165501;
+			public const int tooltip_frame_dark = 2131165501;
+			
+			// aapt resource value: 0x7F07013E
+			public const int tooltip_frame_light = 2131165502;
 			
 			static Drawable()
 			{
